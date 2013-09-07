1328d7bf3661d556b582743aa3a75f9bee7a5a8d
方建 <superman601@126.com>
2013-09-07 13:35:58 -0400

    first commit with maven

---
+++ src/main/java/edu/cmu/courses/ds/MigratableProcess.java
@@ -0,0 +1,9 @@
+package edu.cmu.courses.ds;
+
+public class MigratableProcess implements Runnable{
+    
+    @Override
+    public void run(){
+
+    }
+}
---
+++ src/test/java/edu/cmu/courses/ds/TestMigratableProcess.java
@@ -0,0 +1,5 @@
+package edu.cmu.courses.ds;
+
+public class TestMigratableProcess{
+
+}
